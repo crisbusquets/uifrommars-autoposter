08f70c01ac050fd9c063c57742355086bb317ddd
Cris Busquets <busquets.cris@gmail.com>
2024-12-22 17:33:22 +0100

    Fix Google Workload Identity Federation

--- src/clients/google.js
+++ src/clients/google.js
@@ -1,5 +1,5 @@
 const { google } = require("googleapis");
-const { GoogleAuth } = require("google-auth-library");
+const { UserRefreshClient, GoogleAuth, Impersonated } = require("google-auth-library");
 
 class GoogleSheetsClient {
   constructor() {
@@ -8,15 +8,28 @@ class GoogleSheetsClient {
 
   async initialize() {
     const auth = new GoogleAuth({
+      credentials: {
+        type: "external_account",
+        audience: process.env.GOOGLE_WORKLOAD_IDENTITY_PROVIDER,
+        subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
+        token_url: "https://sts.googleapis.com/v1/token",
+        service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/uifrommars-autoposter@uifrommars-autopost.iam.gserviceaccount.com:generateAccessToken`,
+        credential_source: {
+          url: "https://token.netlify.app/.netlify/functions/scheduled-poster",
+          headers: {},
+          format: {
+            type: "json",
+            subject_token_field_name: "access_token",
+          },
+        },
+      },
       scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-      projectId: "uifrommars-autopost",
-      targetAudience: "https://token.netlify.app",
     });
 
-    const client = await auth.getClient();
+    const authClient = await auth.getClient();
     this.client = google.sheets({
       version: "v4",
-      auth: client,
+      auth: authClient,
     });
   }
 
@@ -31,6 +44,7 @@ class GoogleSheetsClient {
         range: "Posts!A2:D",
       });
 
+      console.log("Sheets response:", response.data);
       return response.data.values || [];
     } catch (error) {
       console.error("Error fetching posts:", error);
